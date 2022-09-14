3fb6420439dcf5c1f822cb592221e227b9e98577
franciscojoaopedro <franciscojoaopedro1998@gmail.com>
2022-09-14 16:53:21 +0100

    js code

--- js/codigo.js
+++ js/codigo.js
@@ -1,2 +1,31 @@
-const KEYAPI="4bd94a36c0791b8a5f607528156c8ca2";
-const KEYAPI1="ee32530e73fc2971189dfad326e208ff";
+const KEYAPI="ee32530e73fc2971189dfad326e208ff"
+const apiCountryFlags="https://countryflagsapi.com/png/";
+
+
+
+const cityInput=document.querySelector('.city-input');
+const btnSearch=document.querySelector('.search');
+
+const cityData={
+    cityElement:document.querySelector('.city'),
+    temperaturaElement:document.querySelector('.temperature span'),
+    descriptionElement:document.querySelector('.description'),
+    weatherIconElement:document.querySelector('.weather-icon'),
+    countryElement:document.querySelector('.country'),
+    umidityElement:document.querySelector('.umidity span'),
+    windElement:document.querySelector('.wind span')
+}
+
+
+const showWeatheData=(city)=>{
+    console.log(city)
+}
+
+
+
+btnSearch.addEventListener('click',(event)=>{
+    event.preventDefault();
+
+    const city=cityInput.value.toLowerCase()
+    console.log(city)
+})
